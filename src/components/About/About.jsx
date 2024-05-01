4b816c129d97c2f0d44d36938830e22f7c7d1df1
Aman Kumar <authorisedaman@gmail.com>
2024-05-01 09:27:30 +0530

    pages

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
 const About = () => {
-  return <div>About</div>;
+  return <div className='flex items-center justify-center h-[59vh]'>This page Under construction!...</div>;
 };
 export default About;
--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div className='flex items-center justify-center h-[59vh]'>
+      This page Under construction!...
+    </div>
+  );
 };
 export default Contact;
